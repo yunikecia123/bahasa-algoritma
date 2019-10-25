15b52d8cab0e13e4b20e57adb4f2f27b65300375
Alicia yunike riona <55614047+yunikecia123@users.noreply.github.com>
2019-10-25 18:07:03 +0700

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI\n\n");
+    printf("NAMA : ALICIA YUNIKE RIONA\n");
+    printf("NPM : 1914370113\n");
+    printf("KELAS : REGULER 1D\n");
+    printf("PRODI : SISTEM KOMPUTER\n");
+    printf("FAKULTAS :SAINS & TEKNOLOGI\n");
+
+
+    int a=10;
+    int b=5;
+    int c;
+    c=a+b;
+    printf("baris 1 -nilai c adalah %d\n",c);
+    c=a-b;
+    printf("baris 2 -nilai c adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai c adalah %d\n",c);
+    c=a/b;
+    printf("baris 4 -nilai c adalah %d\n",c);
+    c= b%a;
+    printf("baris 5 -nilai c adalah %d\n",c);
+    c=a++;
+    printf("baris 6 -nilai c adalah %d\n",c);
+    c=a--;
+    printf("baris 7 -nilai c adalah %d\n",c);
+    return 0;
+}
